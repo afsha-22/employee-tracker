44e5a1be1f5a2bbbbf860ab045546d7e6db0bf1e
Afsha <afsha@afterpaytouch.com>
2021-08-13 16:24:47 +1000

    setup inquirer

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mysql = require('mysql2');
+const table = require('console.table');
+const sequelize = require('./config/connection');
+require("dotenv").config();
+var inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,14 +21,92 @@ const db = mysql.createConnection(
     console.log(`Connected to the books_db database.`)
 );
 
-db.query('SELECT * FROM department', function (err, results) {
-    console.log(results);
-  });
-  
+// db.query('SELECT * FROM department', function (err, results) {
+//     console.table(results);
+//   });
+
+// db.query('SELECT * FROM role', function (err, results) {
+//     console.table(results);
+//   });
+
+// db.query('SELECT * FROM employee', function (err, results) {
+//     console.table(results);
+//   });
+
+
+
+function init() {
+    inquirer.prompt({
+        name: "query",
+        type: 'list',
+        message: 'Please select the below options to run the query and see the results',
+        choices: [
+            "View all employees",
+            "Add employee",
+            "Update employee role",
+            "View all roles",
+            "Add role",
+            "View all departments",
+            "Add department",
+            "Quit",
+        ]
+    })
+    .then((answers) => {
+        switch(answers.query) {
+            case "View all employees":
+                viewAllEmployees();
+                break;
+
+            case "Add employee":
+                addEmployee();
+                break;
+
+            case "Update employee role":
+                updateEmployeeRole();
+                break;
+
+            case "View all roles":
+                viewAllRoles();
+                break;
+
+            case "Add role":
+                addRole();
+                break;
+
+            case "View all departments":
+                viewAllDepartments();
+                break;
+
+            case "Add department":
+                addDepartment();
+                break;
+
+            case "Quit":
+                quit();
+                break;
+        }
+    })
+    // .catch((error) => {
+    //     if (error.isTtyError) {
+    //     // Prompt couldn't be rendered in the current environment
+    //     } else {
+    //     // Something else went wrong
+    //     }
+    // });   
+}
+
+function viewAllEmployees() {
+    db.query('SELECT * FROM department', function (err, results) {
+        console.table(results);
+      });
+}
+
 app.use((req, res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+
+init();
